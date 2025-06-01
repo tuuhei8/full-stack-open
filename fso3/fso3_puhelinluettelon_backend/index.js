3de1f8528a636eb5519e6c6d60412163507bae22
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-06-01 18:12:39 +0300

    baseURL

--- fso3/fso3_puhelinluettelon_backend/index.js
+++ fso3/fso3_puhelinluettelon_backend/index.js
@@ -8,6 +8,7 @@ morgan.token('data', function (req) { return JSON.stringify(req.body)  })
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
+app.use(express.static('dist'))
 
 const generateID = () => {
   const id = Math.floor(Math.random() * 1000)
