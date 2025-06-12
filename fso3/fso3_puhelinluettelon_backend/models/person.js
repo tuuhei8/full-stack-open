d5aeb1e286a6bd2ca0cdfe904e537405e5e93d91
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-06-12 17:56:09 +0300

    korjaus

--- fso3/fso3_puhelinluettelon_backend/models/person.js
+++ fso3/fso3_puhelinluettelon_backend/models/person.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
 mongoose.connect(url)
   .then(() => {
     console.log('connected to MongoDB')
